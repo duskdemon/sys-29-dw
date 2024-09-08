8675e3035e1df8f9317300d5d587df168b9b5511
Dmitriy Dunaev <mb.dusk@gmail.com>
2024-09-08 21:25:00 +0300

    working with script & README.md

--- init2.sh
+++ init2.sh
@@ -4,6 +4,9 @@
 yc alb load-balancer show sys-29-dw-alb | grep address
 BASTHOST=$(terraform output | grep external_ip_bh | awk -F '"' '{print $2}')
 echo basthost ip is : $BASTHOST
-sed -i '2s/.*/{$BASTHOST}/' inventory_bh.ini
+sed -i "2s/.*/$BASTHOST/" inventory_bh.ini
 echo correct ip is set in nventory_bh.ini 
-cat inventory_bh.ini
+KIBANA=$(terraform output | grep external_ip_kb | awk -F '"' '{print $2}')
+echo kibana ip is : $KIBANA
+ZABBIX=$(terraform output | grep external_ip_zb | awk -F '"' '{print $2}')
+echo zabbix ip is : $ZABBIX
